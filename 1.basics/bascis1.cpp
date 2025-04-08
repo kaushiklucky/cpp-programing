ed908095e264884b84636d9ce66ed0d15ebfd618
kaushiklucky <luckykaushik.lbk@gmail.com>
2025-04-08 08:19:27 +0530

    first commit

---
+++ 1.basics/bascis1.cpp
@@ -0,0 +1,21 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+    //printing withaut next line
+    cout<<"Hello India";
+    cout<<"Hello World";
+
+    //printing with next line
+    cout<<"Hello India";
+    cout<<"\n";
+    cout<<"Hello World";
+
+    //printing with next line M-2
+    cout<<"Hello Lucky";
+    cout<<endl;
+    cout<<"Hello Kaushik";
+
+    //printing with next line M-3
+    cout<<"Hello Ara"<<endl<<"Hello Bihar";
+}
