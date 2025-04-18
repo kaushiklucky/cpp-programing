02618ce0d0d06f230bc773bd54ba2073c50a2847
kaushiklucky <luckykaushik.lbk@gmail.com>
2025-04-18 18:31:42 +0530

    first commit

---
+++ 2.ifelse/assingment/numberOfNotes.cpp
@@ -0,0 +1,11 @@
+/*Write a program to count the minimum number of notes 
+in a given amount using the switch statement.*/
+#include<iostream>
+using namespace std;
+int main(){
+    int n, coin1, coin2, coin5;
+    int note10, note20, note50, note100, note200, note500, note2000;
+    cout<<"Enter the number: ";
+    cin>>n;
+    
+}
