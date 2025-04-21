83ba06dfab1e8f21bb637ae0f0c0dc16963e99ad
kaushiklucky <luckykaushik.lbk@gmail.com>
2025-04-21 08:07:37 +0530

    first commit

---
+++ 3.loops/namasteDuniya.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n, i;
+    cout<<"enter the number: ";
+    cin>>n;
+    for(i=1; i<=n; i++){
+        cout<<"Namaste Duniya!!"<<endl;
+    }
+
+}
