dfa1f34805975cba3406db417218d2c03e247b40
kaushiklucky <luckykaushik.lbk@gmail.com>
2025-04-23 15:00:19 +0530

    first commit

---
+++ 4.pattern/flippedTriangle.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+      int n;
+      cout<<"Enter the number : ";
+      cin>>n;
+      for(int i=1; i<=n; i++){
+            //space
+            for(int j=1; j<=n-i; j++){
+                  cout<<" ";
+            }
+
+            //star
+            for(int k=1; k<=i; k++){
+                  cout<<"*";
+            }
+            cout<<endl;
+      }
+
+
+      // //method-2
+      // for(int i=1; i<=n; i++){
+      //       for(int j=1; j<=n; j++){
+      //             if((i+j)>=n+1) cout<<"*";
+      //             else cout<<" ";
+      //       }cout<<endl;
+      // }
+}
