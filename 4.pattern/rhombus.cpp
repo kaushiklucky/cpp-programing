e235f6cd746a6cd7c22f72e8d26aa6ade2f8a8e1
kaushiklucky <luckykaushik.lbk@gmail.com>
2025-05-02 01:19:54 +0530

    first commit

---
+++ 4.pattern/rhombus.cpp
@@ -0,0 +1,34 @@
+#include<iostream> 
+using namespace std;
+int main(){
+      int n;
+      cout<<"Enter the number : ";
+      cin>>n;
+      for(int i=1; i<=n; i++){
+            for(int j=1; j<=n-i; j++){
+                  cout<<" ";
+            }
+            for(int k=1; k<=i; k++){
+                  cout<<"*";
+            }
+            for(int l=1; l<=n+1-i; l++){
+                  cout<<"*";
+            }
+            cout<<endl;
+      }
+
+
+     // method-2
+      for(int i=1; i<=n; i++){
+            //space
+            for(int j=1; j<=n-i; j++){
+                  cout<<" ";
+            }
+
+            //star
+            for(int k=1; k<=n; k++){
+                  cout<<"#";
+            }
+            cout<<endl;
+      }
+}
