0d16abd3cec08bdd41bdc156f8e769224a5b5c66
kaushiklucky <luckykaushik.lbk@gmail.com>
2025-05-02 01:18:25 +0530

    first commit

---
+++ 4.pattern/oddNumberTriangle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+      int n,i,j;
+      cout<<"Enter the number : ";
+      cin>>n;
+      for(i=1; i<=n; i++){
+            for(j=1; j<=2*i-1; j+=2){
+                  cout<<j;
+            }cout<<endl;
+      }
+}
