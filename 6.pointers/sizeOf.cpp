cc2ea726edb437d3ca6be8b29af9c910084cc568
kaushiklucky <luckykaushik.lbk@gmail.com>
2025-05-18 01:40:00 +0530

    first commit

---
+++ 6.pointers/sizeOf.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+      cout<<"size of int is "<<sizeof(int)<<endl;
+      cout<<"size of float is "<<sizeof(float)<<endl;
+      cout<<"size of bool is "<<sizeof(bool)<<endl;
+      cout<<"size of double is "<<sizeof(double)<<endl;
+      cout<<"size of long is "<<sizeof(long)<<endl;
+      cout<<"size of long long is "<<sizeof(long long)<<endl;
+
+
+}
