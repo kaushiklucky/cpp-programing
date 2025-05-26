551c8b9d807774bc5db2ec574a78d079c533aafd
kaushiklucky <luckykaushik.lbk@gmail.com>
2025-05-26 13:23:04 +0530

    first commit

---
+++ 6.pointers/syntaxproblem.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=12,b=10;
+    int *ptr = &a, *ptr2 = &b;
+    int x=4,y=6,z=9;
+    int* p1 = &x, p3=z;
+    int* p2 = &y;
+
+//star associated with int consider first variable as pointer and second variable as int
+//here z is int
+}
