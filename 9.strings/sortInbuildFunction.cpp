98ab447bc4c9eeb7c6fd08cbb7bd87bfef9b2a75
kaushiklucky <luckykaushik.lbk@gmail.com>
2025-08-10 18:28:31 +0530

    first commit

---
+++ 9.strings/sortInbuildFunction.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+      string s;
+      cout<<"Enter string : ";
+      getline(cin, s);
+      sort(s.begin(),s.end());
+      cout<<s;
+}
