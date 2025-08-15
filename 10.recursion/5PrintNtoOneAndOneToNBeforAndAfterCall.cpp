9185669395ccc800015eab71d48c9ba04d9cf2ce
kaushiklucky <luckykaushik.lbk@gmail.com>
2025-08-15 12:22:06 +0530

    first commit

---
+++ 10.recursion/5PrintNtoOneAndOneToNBeforAndAfterCall.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+void print(int n){
+      if(n==0) return; //base case
+      cout<<n<<endl; // kaam
+      print(n-1); // call
+      cout<<n<<endl; // kaam
+}
+int main(){
+      int n;
+      cout<<"Enter n : "; cin>>n;
+      print(n);
+}
