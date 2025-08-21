a3d34595ac2243966eb9ca9d662a97d39189821e
kaushiklucky <luckykaushik.lbk@gmail.com>
2025-08-21 18:10:17 +0530

    first commit

---
+++ 10.recursion/16zigZagParttern.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+void zigZag(int n){
+    if(n==0) return;
+    cout<<n;
+    zigZag(n-1);
+    cout<<n;
+    zigZag(n-1);
+    cout<<n;
+}
+int main(){
+    cout<<"Enter n: ";
+    int n;
+    cin>>n;
+    zigZag(n);
+}
