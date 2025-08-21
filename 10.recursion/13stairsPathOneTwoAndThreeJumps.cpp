2c5e2a8de6cdbc14e30fb51cd9cb8b225a31da6e
kaushiklucky <luckykaushik.lbk@gmail.com>
2025-08-21 17:46:11 +0530

    first commit

---
+++ 10.recursion/13stairsPathOneTwoAndThreeJumps.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int stairs(int n){
+    if(n==3) return 3;
+    if(n==2) return 2;
+    if(n==1) return 1;
+    return stairs(n-1) + stairs(n-2) + stairs(n-3);
+}
+int main(){
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+    cout<<stairs(n);
+}
