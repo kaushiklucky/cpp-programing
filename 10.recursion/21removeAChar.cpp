ead5880b54da727af20e454f177cd3f7097d1a39
kaushiklucky <luckykaushik.lbk@gmail.com>
2025-08-23 18:07:46 +0530

    first commit

---
+++ 10.recursion/21removeAChar.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+void removeChar(string ans, string original){
+    if(original.length()==0){
+        cout<<ans;
+        return;
+    }
+    char ch = original[0];
+    if(ch=='k') removeChar(ans, original.substr(1));
+    else removeChar(ans+ch, original.substr(1));
+
+}
+int main(){
+    string s = "Lucky Kaushik";
+    removeChar("",s);
+}
