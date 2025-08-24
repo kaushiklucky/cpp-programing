54747dd228cba76826e03c78f581479d6fad6e41
kaushiklucky <luckykaushik.lbk@gmail.com>
2025-08-24 12:48:16 +0530

    first commit

---
+++ 10.recursion/22removeACharOptimized.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void removeChar(string ans, string original, int idx){
+    if(idx==original.length()){
+        cout<<ans;
+        return;
+    }
+    char ch = original[idx];
+    if(ch=='k') removeChar(ans, original, idx+1);
+    else removeChar(ans+ch, original, idx+1);
+}
+int main(){
+    string s = "Lucky Kaushik";
+    removeChar("",s,0);
+}
